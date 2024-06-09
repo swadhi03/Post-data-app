6f68c8d6a098a5e9d5efc3ae7efffbc73cc3a480
swadhi03 <swathiksanthosh03@gmail.com>
2024-06-09 23:18:11 +0530

    completed

--- src/components/SearchPost.jsx
+++ src/components/SearchPost.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const SearchPost = () => {
+    const [data,changeData]=useState(
+        {
+            "Name" :""
+        }
+    )
+    const inputHandler=(event)=>{
+        changeData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
         <div className="container">
@@ -9,10 +20,10 @@ const SearchPost = () => {
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label htmlFor="" className="form-label">Name</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" name='Name' value={data.name} onChange={inputHandler}/>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button className="btn btn-success">Search</button>
+                    <button className="btn btn-success" onClick={readValue}>Search</button>
                 </div>
             </div>
         </div>
